a91b0a68160851ce1f9789ff023fa85c0720745b
DeeptiVarghese <31417684+DeeptiVarghese@users.noreply.github.com>
2024-05-16 00:47:23 -0700

    Update query_1.sql

---
+++ submission/query_1.sql
@@ -0,0 +1,20 @@
+create table actors(
+actor varchar not null,
+actor_id varchar not null,
+films array(    --struct array for storing film variables for the actor
+row(
+year integer,  
+film varchar,
+votes integer,
+rating double,
+film_id varchar
+)),
+quality_class varchar,   --categorical variable for binning average ranges, hence varchar
+is_active boolean,
+current_year integer
+)
+with
+  (
+    format = 'PARQUET',
+    partitioning = ARRAY['current_year']
+  )
